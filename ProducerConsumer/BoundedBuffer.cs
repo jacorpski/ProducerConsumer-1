30052906bd14876fb7c3345bc4a44673a8358a18
Jacob Trier Christensen <jacorpski@gmail.com>
2014-09-18 10:14:50 +0200

    Created the BoundedBuffer class

---
+++ ProducerConsumer/BoundedBuffer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    public class BoundedBuffer
+    {
+        public BoundedBuffer(int capacity)
+        {
+            
+        }
+
+        public Boolean IsFull()
+        {
+            
+        }
+
+        public void Put(int element)
+        {
+            
+        }
+
+        public int Take()
+        {
+            
+        }
+    }
+}
